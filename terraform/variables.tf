1261cff3d9f7c636daaeb989043ff9e94d9b4d42
ronflex77 <116546506+ronflex77@users.noreply.github.com>
2026-03-30 17:37:21 +0200

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,5 +1,20 @@
-variable "render_api_key" { type = string; sensitive = true }
-variable "render_owner_id" { type = string }
-variable "image_url" { type = string }
-variable "image_tag" { type = string }
-variable "github_actor" { type = string }
+variable "render_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "render_owner_id" {
+  type = string
+}
+
+variable "image_url" {
+  type = string
+}
+
+variable "image_tag" {
+  type = string
+}
+
+variable "github_actor" {
+  type = string
+}
